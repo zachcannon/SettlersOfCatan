c6c560241b2d5e5adf17bfe69deb41d7f82d5ef1
Zachary Cannon <zach.cannon10@gmail.com>
2013-07-21 17:58:31 -0500

    Updated Scripts and Resources

---
+++ SettlersOfCatan/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SettlersOfCatan.Controllers
+{
+    public class GameController : Controller
+    {
+
+        //Display 4 Players with with Resources below
+
+        public ActionResult SettlersGame()
+        {
+            return View();
+        }
+
+    }
+}
