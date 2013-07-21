3d65c8ec4ee8f37262fbc116af5137935c0f97b9
Zachary Cannon <zach.cannon10@gmail.com>
2013-07-20 22:05:13 -0500

    Created Die Class which random rolls

---
+++ SettlersOfCatan/Controllers/StartGameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SettlersOfCatan.Models;
+
+namespace SettlersOfCatan.Controllers
+{
+    public class StartGameController : Controller
+    {
+        
+        public ActionResult DieRollPage()
+        {
+            Die dieToRoll = new Die();
+            dieToRoll.rollDie();
+            return View(dieToRoll);
+        }
+
+    }
+}
---
+++ SettlersOfCatan/Models/Die.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SettlersOfCatan.Models
+{
+    public class Die
+    {
+        public int dieValue { get; set; }
+
+        public void rollDie()
+        {
+            Random random = new Random();
+            this.dieValue = random.Next(1, 7);
+        }
+    }
+}
